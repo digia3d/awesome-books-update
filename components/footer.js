80dfccd844bd45c2cf61262a5a52858e4ac08f75
Aleksandra <ujvari65@gmail.com>
2025-07-28 22:55:47 +0200

    ✨ Added Luxon magic to footer — now showing real-time date and time!

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,14 @@
+import { getCurrentFormattedDate } from '../modules/luxon.js';
+
+
 const createFooter = () => {
   const footer = document.createElement('footer');
   footer.className = 'footer';
 
   const year = new Date().getFullYear();
-  footer.innerHTNL = `<p>@ ${year} Awesome Books. All rights reserved.</p>`;
+  const currentDate = getCurrentFormattedDate();
+
+  footer.innerHTML = `<p>@ ${year} Awesome Books. All rights reserved.</p><p>Current Date and Time: ${currentDate}<p>`;
 
   document.body.appendChild(footer);
 };
