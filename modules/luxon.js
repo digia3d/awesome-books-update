e4849e4566a20a15f1f763c44dc6d1b06a651fb5
Aleksandra Ujvari <ujvari65@gmail.com>
2026-01-04 23:41:07 +0100

    Fix import for Luxon
    Fix import for DateTime in luxon.js

--- modules/luxon.js
+++ modules/luxon.js
@@ -1,6 +1,5 @@
 // modules/luxon.js
-import { DateTime } from 'luxon'; // ovo koristi ES6 import iz node_modules
-
 export const getCurrentFormattedDate = () => {
-  return DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+  return luxon.DateTime.now().toLocaleString(luxon.DateTime.DATETIME_MED_WITH_SECONDS);
 };
+
