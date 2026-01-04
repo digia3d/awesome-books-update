834bda2dc9c1e75b29b033ac624f56f043069339
Aleksandra <ujvari65@gmail.com>
2026-01-04 23:26:57 +0100

    Fix linters error

--- modules/luxon.js
+++ modules/luxon.js
@@ -1,6 +1,6 @@
 // modules/luxon.js
-const DateTime = luxon.DateTime;
+import { DateTime } from 'luxon'; // ovo koristi ES6 import iz node_modules
 
 export const getCurrentFormattedDate = () => {
   return DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
-}
+};
